8669f1c60c04c2ab213a663b0f45e97d280626c4
nho-moi-so <lythuymyngoc310@gmail.com>
2024-11-14 21:38:28 +0700

    doi avt

--- doc/form-add-don-hang.php
+++ doc/form-add-don-hang.php
@@ -37,7 +37,7 @@
   <!-- Sidebar menu-->
   <div class="app-sidebar__overlay" data-toggle="sidebar"></div>
   <aside class="app-sidebar">
-    <div class="app-sidebar__user"><img class="app-sidebar__user-avatar" src="../images/hay.jpg" width="50px"
+    <div class="app-sidebar__user"><img class="app-sidebar__user-avatar" src="../img-sanpham/avatar-trang-2.jpg" width="50px"
         alt="User Image">
       <div>
         <p class="app-sidebar__user-name"><b>Võ Trường</b></p>
